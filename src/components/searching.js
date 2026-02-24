248f94b56b6801f46dbf7aa13d9780f308a99c99
Alexandr Sergeev <sergeevrut@yandex.ru>
2026-02-24 19:58:58 +0300

    final version 1

--- src/components/searching.js
+++ src/components/searching.js
@@ -1,6 +1,3 @@
-import {rules, createComparison} from "../lib/compare.js";
-
-
 export function initSearching(searchField) {
     return (query, state, action) => { // result заменили на query
         return state[searchField] ? Object.assign({}, query, { // проверяем, что в поле поиска было что-то введено
