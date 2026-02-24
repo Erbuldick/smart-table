8b648f5220898ee046392e01d4a6817cb6a97096
Alexandr Sergeev <sergeevrut@yandex.ru>
2026-02-24 19:44:07 +0300

    final version

--- src/main.js
+++ src/main.js
@@ -1,27 +1,41 @@
 import './fonts/ys-display/fonts.css'
 import './style.css'
 
-import {data as sourceData} from "./data/dataset_1.js";
-
 import {initData} from "./data.js";
 import {processFormData} from "./lib/utils.js";
 
 import {initTable} from "./components/table.js";
 // @todo: подключение
+import { initPagination } from "./components/pagination.js";
+import { initSorting } from "./components/sorting.js";
+import { initFiltering } from "./components/filtering.js";
+import { initSearching } from "./components/searching.js";
 
 
 // Исходные данные используемые в render()
-const {data, ...indexes} = initData(sourceData);
+const api = initData();
 
 /**
  * Сбор и обработка полей из таблицы
  * @returns {Object}
  */
 function collectState() {
-    const state = processFormData(new FormData(sampleTable.container));
+    const { rowsPerPage, page, totalFrom, totalTo, ...state } = processFormData(new FormData(sampleTable.container));
+    const rowsPerPageInt = parseInt(rowsPerPage);
+    const pageInt = parseInt(page ?? 1);
+    const total = [null,  null];
+    const totalFromNum = Number(totalFrom);
+    if (totalFrom !== '' && !isNaN(totalFromNum))
+        total[0] = totalFromNum;
+    const totalToNum = Number(totalTo);
+    if (totalTo !== '' && !isNaN(totalTo))
+        total[1] = totalToNum;
 
     return {
-        ...state
+        ...state,
+        rowsPerPage: rowsPerPageInt,
+        page: pageInt,
+        total,
     };
 }
 
@@ -29,26 +43,58 @@ function collectState() {
  * Перерисовка состояния таблицы при любых изменениях
  * @param {HTMLButtonElement?} action
  */
-function render(action) {
+async function render(action) {
     let state = collectState(); // состояние полей из таблицы
-    let result = [...data]; // копируем для последующего изменения
-    // @todo: использование
-
-
-    sampleTable.render(result)
+    let query = {}; // копируем для последующего изменения
+    query = applyPagination(query, state, action);
+    query = applyFiltering(query, state, action);
+    query = applySearching(query, state, action);
+    query = applySorting(query, state, action);
+    const { total, items } = await api.getRecords(query);
+    updatePagination(total, query);
+    sampleTable.render(items)
 }
 
 const sampleTable = initTable({
     tableTemplate: 'table',
     rowTemplate: 'row',
-    before: [],
-    after: []
+    before: ['search', 'header', 'filter'],
+    after: ['pagination']
 }, render);
 
 // @todo: инициализация
+const {applyPagination, updatePagination} = initPagination(
+    sampleTable.pagination.elements,
+    (el, page, isCurrent) => {
+        const input = el.querySelector('input');
+        const label = el.querySelector('span');
+        input.value = page,
+        input.checked = isCurrent;
+        label.textContent = page;
+        return el;
+    }
+)
+
+const applySorting = initSorting([        
+    sampleTable.header.elements.sortByDate,
+    sampleTable.header.elements.sortByTotal
+]);
+
+const {applyFiltering, updateIndexes } = initFiltering(
+    sampleTable.filter.elements);
+
+const applySearching = initSearching('search');
 
 
 const appRoot = document.querySelector('#app');
 appRoot.appendChild(sampleTable.container);
 
-render();
+const init = async () => {
+    const indexes = await api.getIndexes();
+
+    updateIndexes(sampleTable.filter.elements, {
+        searchBySeller: indexes.sellers
+    });
+}
+
+init().then(render)
